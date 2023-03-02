d3153ed3c3c19eefac331a6109ea176d978c8a05
abhishekkhanna2710 <abhikhanna2710@gmail.com>
2023-03-02 20:52:49 +0530

    landing_page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Navbar from './Components/Navbar/Navbar';
+import Landing from './Components/Landing_page/Landing';
+import Footer from "./Components/Footer/Footer"
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Landing />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
